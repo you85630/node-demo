e0e16367595b5dafe56cdbdba221dc0d2bb6ed6b
You <you85630@foxmail.com>
2017-12-18 14:01:55 +0800

    init

--- server/api.js
+++ server/api.js
@@ -1,12 +1,8 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send('home page')
-})
-
-router.get('/one', function(req, res) {
-  res.send('page one')
 })
 
 module.exports = router
