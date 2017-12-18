11341316c8b158ef2a104c7d93d7380290cc51c0
You <you85630@foxmail.com>
2017-12-18 18:06:06 +0800

    init

--- server/api.js
+++ server/api.js
@@ -2,15 +2,23 @@ const fs = require('fs')
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const bodyParser = require('body-parser')
 
+// 创建 application/x-www-form-urlencoded 编码解析
+const BParser = bodyParser.urlencoded({ extended: false })
+
+// 路径
 const resolve = file => path.resolve(__dirname, file)
 
 router.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.sendFile(resolve('./../index.html'))
 })
 
 router.get('/newsList', function(req, res) {
-  fs.readFile(resolve('newsList.json'), 'utf8', function(err, data) {
+  fs.readFile(resolve('./../public/newsList.json'), 'utf8', function(
+    err,
+    data,
+  ) {
     if (err) {
       let errData = {
         code: '400',
@@ -18,9 +26,17 @@ router.get('/newsList', function(req, res) {
       }
       res.send(errData)
     } else {
-      res.send(data.toString())
+      res.send(data)
     }
   })
+})
+
+router.post('/formPost', BParser, function(req, res) {
+  let formData = {
+    fname: req.body.fname,
+    lname: req.body.lname,
+  }
+  res.send(formData)
 })
 
 module.exports = router
