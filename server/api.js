588eaf8c3987636ca13c4eea6d58c7332cacbaac
You <you85630@foxmail.com>
2017-12-18 16:18:09 +0800

    init

--- server/api.js
+++ server/api.js
@@ -1,13 +1,16 @@
-const express = require('express')
 const fs = require('fs')
+const express = require('express')
 const router = express.Router()
+const path = require('path')
+
+const resolve = file => path.resolve(__dirname, file)
 
 router.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
 router.get('/newsList', function(req, res) {
-  fs.readFile('./../public/newsList.json', 'utf8', function(err, data) {
+  fs.readFile(resolve('newsList.json'), 'utf8', function(err, data) {
     if (err) {
       let errData = {
         code: '400',
