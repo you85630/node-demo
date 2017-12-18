1474156c5569da55e7318d4be031aef8cad02d5f
You <you85630@foxmail.com>
2017-12-18 15:46:04 +0800

    init

--- server/api.js
+++ server/api.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 
 router.get('/', function(req, res) {
-  res.send('home page')
+  res.send('Hello World!')
+})
+
+router.get('/newsList', function(req, res) {
+  let newsList = fs.readFile('./../public/newsList.json', function(err, data) {
+    if (err) {
+      let errData = {
+        code: '400',
+        message: '数据接收失败',
+      }
+      res.send(errData)
+    } else {
+      res.send(data.toString())
+    }
+  })
 })
 
 module.exports = router
