6933548b62c48364bee19fa650e7085300a7a48f
You <you85630@foxmail.com>
2017-12-18 14:14:10 +0800

    init

--- server/index.js
+++ server/index.js
@@ -4,19 +4,19 @@ const api = require('./api')
 
 app.use(api)
 
-//端口设为3000端口
+// 端口设为3000端口
 app.set('port', process.env.port || 3000)
 
-//CORS跨域配置
+// CORS跨域配置
 app.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*') // 这个表示任意域名都可以访问，这样写不能携带cookie了
   res.header(
     'Access-Control-Allow-Headers',
     'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
   )
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS') // 设置方法
   if (req.method === 'OPTIONS') {
-    res.sendStatus(200)
+    res.send(200) // 在正常的请求之前， 发送一个验证， 是否可以请求
   } else {
     next()
   }
