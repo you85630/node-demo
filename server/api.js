677f4c5445eb7ad1e0c8638c12ae503a364f4880
You <you85630@foxmail.com>
2017-12-19 15:34:04 +0800

    init

--- server/api.js
+++ server/api.js
@@ -1,28 +1,21 @@
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
+const path = require('path')
 const router = express.Router()
 const db = require('./db')
 
 // 路径
 const resolve = file => path.resolve(__dirname, file)
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   res.sendFile(resolve('./../index.html'))
 })
 
 router.get('/newsList', (req, res) => {
-  db.posts.find((err, doc) => {
+  db.newsList.find({}, (err, doc) => {
     if (err) {
-      let errData = {
-        code: '400',
-        message: '数据接收失败',
-      }
-      res.send(errData)
-      return
+      res.send(err)
     } else {
-      console.log(data)
-      res.send(data)
+      res.send(doc)
     }
   })
 })
